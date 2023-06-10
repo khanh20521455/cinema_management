40a92273c61d735baf02d9b07c27dd1459837238
Khanh20521455 <20521455@gm.uit.edu.vn>
2023-06-10 10:36:25 +0700

    showtimes_managemnet

--- cinema_management/src/main/java/cinema_management/entities/Showtimes.java
+++ cinema_management/src/main/java/cinema_management/entities/Showtimes.java
@@ -1,7 +1,10 @@
 package cinema_management.entities;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
+import java.sql.Date;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,9 +20,70 @@ public class Showtimes {
     @ManyToOne
     @JoinColumn(name="roomId")
     private Room room;
-    private LocalDateTime time;
+    private String time;
+    private Date date;
     private int price;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "showtimes")
     private List<Booking> bookingList;
+
+    public Showtimes() {
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public List<Booking> getBookingList() {
+        return bookingList;
+    }
+
+    public void setBookingList(List<Booking> bookingList) {
+        this.bookingList = bookingList;
+    }
+
+
 
 }
--- cinema_management/src/main/java/cinema_management/repository/ShowtimesRepository.java
+++ cinema_management/src/main/java/cinema_management/repository/ShowtimesRepository.java
@@ -1,11 +1,18 @@
 package cinema_management.repository;
 
+import cinema_management.entities.Room;
 import cinema_management.entities.Showtimes;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.Entity;
 
 @Repository
 public interface ShowtimesRepository extends JpaRepository<Showtimes, Integer> {
+    @Query("FROM Showtimes as s ORDER  BY s.date")
+    public Page<Showtimes> findAllOrderByDateAsc(Pageable pageable);
+
 }
